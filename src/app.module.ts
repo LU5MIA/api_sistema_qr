27d9d5f85d93364854fd7c7e9eb3b47c75b0e624
LUCERO <lucerorodriguezestacio@gmail.com>
2025-12-09 08:50:20 -0500

    nuevos cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { ParticipantesModule } from './participantes/participantes.module';
 import { SesionesModule } from './sesiones/sesiones.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '', 
-      database: 'sistema_qr',
-      synchronize: false, 
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+      autoLoadEntities: true,
     }),
+
     ParticipantesModule,
     SesionesModule,
   ],
