713b6b69ef93f6001ac2c4a19210b39d5141c468
LUCERO <lucerorodriguezestacio@gmail.com>
2025-12-14 05:52:06 -0500

    cambios en create dto

--- src/sesiones/dto/create-sesiones.dto.ts
+++ src/sesiones/dto/create-sesiones.dto.ts
@@ -1,8 +1,6 @@
 import { IsDate, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class createSesionesDto {
-    @IsNumber()
-    id_sesion: number;
 
     @IsString()
     @IsNotEmpty()
--- src/usuarios/dto/create-usuarios.dto.ts
+++ src/usuarios/dto/create-usuarios.dto.ts
@@ -1,10 +1,7 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateUsuariosDto {
-
-    @IsNumber()
-    id_usuario: number;
-
+    
     @IsString()
     @IsNotEmpty()
     nombre_usuario: string;
