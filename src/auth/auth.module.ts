c66f0e1b97f0a7622afa93d89f172d4d531e0101
LUCERO <lucerorodriguezestacio@gmail.com>
2025-12-14 04:13:55 -0500

    Importaciones en usuarios y auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuarios } from 'src/usuarios/entities/usuarios.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Usuarios])],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuarios } from './entities/usuarios.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Usuarios])],
   providers: [UsuariosService],
   controllers: [UsuariosController]
 })
-export class UsuariosModule {}
+export class UsuariosModule {} 
